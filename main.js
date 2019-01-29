d172fecc55ca588123a1154e371b1386f378bbca
NaughtyPotato <katarina26replays@gmail.com>
2019-01-29 12:55:04 +0100

    Minor tweaks

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ sessionuuid(); //Generate a uuid to be used as api.ai sessionId
 //#endregion
 
 Discord_Client.on('ready', () => {
-    console.log(Discord_Client.user.tag,new Date()); //Console Log -BotName #0000- -Time of Log in-
+    console.log(Discord_Client.user.tag,new Date(),sessionuuid()); //Console Log -BotName #0000- -Time of Log in- -sessionId-
 
     Discord_Client.user.setPresence({
         game: { 
@@ -26,7 +26,7 @@ Discord_Client.on('ready', () => {
 
 Discord_Client.on('message', message => {
     
-    if(message.content != null && message.author.bot == false){
+    if(message.content != null && !message.author.bot){
 
         var request = Apiai_Client.textRequest(message.content, {
             sessionId: sessionuuid()
@@ -35,14 +35,13 @@ Discord_Client.on('message', message => {
         request.on('response', response => {
             console.log(response); //Console Log response data in JSON format
 
-            //var respond = response.result.fulfillment.speech;
-            var stringRespond = JSON.stringify(response.result.fulfillment.speech);
-            stringRespond = stringRespond.substring(1,stringRespond.length -1);
-            message.channel.send(stringRespond);
+            var respond = JSON.stringify(response.result.fulfillment.speech);
+            respond = respond.substring(1,respond.length -1);
+            message.channel.send(respond);
         });
 
         request.on('error', error => {
-            console.log(error);
+            console.log(error); //Console Log any errors
         });
     
         request.end();
